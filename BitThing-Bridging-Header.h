20842902d47843f334162358c72deafa1084997f
Matthew Ernst <matthew.f.ernst@gmail.com>
2022-03-20 17:33:28 -0700

    working proto

---
+++ BitThing-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  BitThing-Bridging-Header.h
+//  BitThing
+//
+//  Created by Matthew Ernst on 3/20/22.
+//
+
+#ifndef BitThing_Bridging_Header_h
+#define BitThing_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* BitThing_Bridging_Header_h */
